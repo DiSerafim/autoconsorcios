5c9a398b9fb4368b86020652bee9a0296312366f
Diego Serafim <diegoserafim1@gmail.com>
2022-08-24 18:01:14 -0300

    criado as rotas para as paginas

--- src/App.js
+++ src/App.js
@@ -2,7 +2,18 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
+import Administrativo from './Pages/Administrativo';
+import Callcenter from './Pages/Callcenter';
+import Diretoria from './Pages/Diretoria';
+import Dp from './Pages/Dp';
+import Ferramentas from './Pages/Ferramentas';
+import Financeiro from './Pages/Financeiro'; 
 import Home from './Pages/Home';
+import Marketing from './Pages/Marketing';
+import Parceiros from './Pages/Parceiros';
+import Recepcao from './Pages/Recepcao';
+import ServicosGerais from './Pages/ServicosGerais';
+import Ti from './Pages/Ti';
 
 function App() {
   return (
@@ -10,7 +21,18 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route path='/' component={Home} />
+          <Route path='/administrativo' element={<Administrativo />} />
+          <Route path='/callcenter' element={<Callcenter />} />
+          <Route path='/diretoria' element={<Diretoria />} />
+          <Route path='/dp' element={<Dp />} />
+          <Route path='/ferramentas' element={<Ferramentas />} />
+          <Route path='/financeiro' element={<Financeiro />} />
+          <Route path='/' element={<Home />} />
+          <Route path='/marketing' element={<Marketing />} />
+          <Route path='/parceiros' element={<Parceiros />} />
+          <Route path='/recepcao' element={<Recepcao />} />
+          <Route path='/servicosGerais' element={<ServicosGerais />} />
+          <Route path='/ti' element={<Ti />} />
         </Routes>
       </Router>
     </>
